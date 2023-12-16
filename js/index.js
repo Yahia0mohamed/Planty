2dda33284d9f728ec56512719d53eb7604b3b1e7
yahia mohamed nabil <99214644+Yahia0mohamed@users.noreply.github.com>
2023-12-16 18:51:49 +0200

    some changes for the buttons

--- js/index.js
+++ js/index.js
@@ -2,6 +2,14 @@ function goToAbout(){
     window.location.href='../html/about.html';
 }
 
+var abtBtn=document.getElementById('abtBtn');
+
+abtBtn.addEventListener('click',goToAbout);
+
 function goToLogin(){
     window.location.href='../html/login.html';
 }
+
+var lgBtn=document.getElementById('lgBtn');
+
+lgBtn.addEventListener('click',goToLogin);
